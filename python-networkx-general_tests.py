bd267438af8a1d7c521ef8d9206c055e14595a8f
pagai <pagai@phobos.fritz.box>
2021-05-28 08:36:37 +0200

    latest changes

--- python-networkx-general_tests.py
+++ python-networkx-general_tests.py
@@ -35,72 +35,7 @@ def get_column_names(filereader):
 def remove_doubles(inputlist):
     return list(set(inputlist))
 
-### NODELINKDATA
-def import_node_link_data_to_graph(inputfile):
-    print("Importing Node Link Data")
-    file_to_read = open(inputfile, 'r')
-    json_data = json.loads(file_to_read.read())    
-    return json_graph.node_link_graph(json_data, directed=True, multigraph=False)
 
-def export_graph_to_node_link_data(G,outputfile):
-    print("Exporting graph to node_link_data-file")
-    file_to_write = open(outputfile, 'w')
-    file_to_write.write(json.dumps(json_graph.node_link_data(G)))
-### NODELINKDATA
-
-#### GRAPHML
-def export_graph_to_graphML_data(G,outputfile):
-    print("Exporting to graphML")
-    nx.write_graphml(G, outputfile, prettyprint=True )
-
-def import_graphML_to_graph(inputfile):
-    print("Importing graphML file")
-    return nx.read_graphml(inputfile)
-    
-#### GRAPHML
-
-#### ADJLIST
-def export_graph_to_adjlist_data(G,outputfile):
-    print("Exporting graph to normal Adj List")
-    nx.write_adjlist(G, outputfile, delimiter=',')
-
-def import_adjlist_to_graph(inputfile):
-    print("Importing normal Adj List")
-    return nx.read_adjlist(inputfile, delimiter=',')
-#### ADJLIST
-
-#### MULTILINE ADJLIST
-def export_graph_to_multiline_adjlist_data(G,outputfile):
-    print("Exporting graph to Multiline Adj List")
-    nx.write_multiline_adjlist(G, outputfile, delimiter=',')
-
-def import_multiline_adjlist_to_graph(inputfile):
-    print("Importing Multiline Adj List")
-    G = nx.read_multiline_adjlist(inputfile,delimiter=',',create_using=nx.DiGraph)
-    return G
-#### MULTILINE ADJLIST
-
-#### YAML
-def export_graph_to_yaml_data(G,outputfile):
-    print("Exporting graph to YAML")
-    nx.write_yaml(G, outputfile)
-
-def import_yaml_to_graph(inputfile):
-    print("Importing YAML")
-    G = nx.read_yaml(inputfile)
-    return G
-#### YAML
-
-#### GML
-def export_graph_to_gml_data(G,outputfile):
-    print("Exporting graph to GML")
-    nx.write_gml(G, outputfile)
-
-def import_gml_to_graph(inputfile):
-    print("Importing GML")
-    G = nx.read_gml(inputfile)
-    return G
-#### GML
 
 
 def print_all(G):
@@ -179,7 +114,7 @@ def create_graph_from_neo4j_csv(G,filePath):
 #create_watts_strogatz_graph()
 #create_graph_from_a_list(10000000)
 
-exportFiles = True
+exportFiles = False
 if (exportFiles):
     G = nx.DiGraph()
     filepath='/home/pagai/graph-data/cooccsdatabase/cooccsdb.csv'
@@ -217,47 +152,47 @@ if (exportFiles):
 importFiles = True
 if (importFiles):
 # IMPORT FILES
-    start_import_time = time.time()
-    G = import_node_link_data_to_graph('/var/tmp/export_01_node_link_data.json')
-    print("Finished in : " + to_ms(time.time() - start_import_time))
-    print_all(G)
-    print("=================")
-    del G
-    
-    start_import_time = time.time()
-    G = import_graphML_to_graph('/var/tmp/export_02_graphML.json')
-    print("Finished in : " + to_ms(time.time() - start_import_time))
-    print_all(G)
-    print("=================")
-    del G
-
-    start_import_time = time.time()
-    G = import_adjlist_to_graph('/var/tmp/export_03_adjlist.txt')
-    print("Finished in : " + to_ms(time.time() - start_import_time))
-    print_all(G)
-    print("=================")
-    del G
-        
-    start_import_time = time.time()
-    G = import_multiline_adjlist_to_graph('/var/tmp/export_04_multiline_adjlist.txt')
-    print("Finished in : " + to_ms(time.time() - start_import_time))
-    print_all(G)
-    print("=================")
-    del G
-       
 #    start_import_time = time.time()
-#    G = import_yaml_to_graph('/var/tmp/export_05_yaml.yaml')
+#    G = import_node_link_data_to_graph('/var/tmp/export_01_node_link_data.json')
 #    print("Finished in : " + to_ms(time.time() - start_import_time))
 #    print_all(G)
 #    print("=================")
 #    del G
-    
+#    
+#    start_import_time = time.time()
+#    G = import_graphML_to_graph('/var/tmp/export_02_graphML.json')
+#    print("Finished in : " + to_ms(time.time() - start_import_time))
+#    print_all(G)
+#    print("=================")
+#    del G
+#
+#    start_import_time = time.time()
+#    G = import_adjlist_to_graph('/var/tmp/export_03_adjlist.txt')
+#    print("Finished in : " + to_ms(time.time() - start_import_time))
+#    print_all(G)
+#    print("=================")
+#    del G
+#        
+#    start_import_time = time.time()
+#    G = import_multiline_adjlist_to_graph('/var/tmp/export_04_multiline_adjlist.txt')
+#    print("Finished in : " + to_ms(time.time() - start_import_time))
+#    print_all(G)
+#    print("=================")
+#    del G
+       
     start_import_time = time.time()
-    G = import_gml_to_graph('/var/tmp/export_06_gml.gml')
-    print("Finished in : " + to_ms(time.time() - start_import_time))   
+    G = import_yaml_to_graph('/var/tmp/export_05_yaml.yaml')
+    print("Finished in : " + to_ms(time.time() - start_import_time))
     print_all(G)
     print("=================")
     del G
+    
+#    start_import_time = time.time()
+#    G = import_gml_to_graph('/var/tmp/export_06_gml.gml')
+#    print("Finished in : " + to_ms(time.time() - start_import_time))   
+#    print_all(G)
+#    print("=================")
+#    del G
         
 
 #G = import_yaml_to_graph('/var/tmp/node_link_data_cooccs_to_yaml.yaml')
